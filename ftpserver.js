d60026a408aac46c3572825c650a2d90af89d9f2
Gowthamr221 <gowthamr221@gmail.com>
2021-11-18 17:40:18 +0530

    upload

--- ftpserver.js
+++ ftpserver.js
@@ -1,13 +1,40 @@
 const express = require('express')
 const serveIndex = require('serve-index')
+const ejs=require('ejs')
+
+const multer  = require('multer')
+const bodyParser = require('body-parser');
+
+
 
 const app = express()
-
+const Storage = multer.diskStorage({
+    destination: "./public/ftp/",
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + file.originalname);
+    },
+  });
+  
+  var upload = multer({
+    storage: Storage,
+  }).single("uploaded_file");
+  
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
-    '/',
-    express.static('public/ftp'),
+    '/ftp',express.static('public/ftp'),
     serveIndex('public/ftp',{icons:true})
 )
-
-
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/upload',(req,res)=>{
+    res.render('upload');
+})
+app.post('/upload',upload,(req,res)=>{
+    res.redirect('/')
+   
+})
 app.listen(3000,()=>console.log('✌ server running on port 3000'))
